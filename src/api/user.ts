470bdeb48d972b3af05e2bf5fb558bf193c8bb8f
killer_xzy <killer_xzy@sina.com>
2020-07-16 23:12:10 +0800

    公共数据请求方法

---
+++ src/api/user.ts
@@ -0,0 +1,9 @@
+/*
+ * @Company: Inossem
+ * @Author: zhanyu
+ * @Email: zhanyu.xu@inossem.com
+ * @Date: 2020-07-16 23:08:55
+ * @LastEditors: zhanyu
+ * @LastEditTime: 2020-07-16 23:09:04
+ * @Description: description
+ */
